5f4a9e563a96d1ac754d82a3c0ba5f5e593d645f
ar[arnau321 <arnau123@att.net>
2021-10-19 13:45:31 -0400

    Add another Trie implimentation

---
+++ Tries/Trie-class-children-imp.js
@@ -0,0 +1,49 @@
+class Node {
+  constructor(){
+    this.children = {}
+    this.isWordEnd = false
+  }
+}
+
+class Trie {
+  constructor(){
+    this.root = new Node()
+  }
+
+  insert(word){
+    let curr = this.root
+    for(let i = 0; i < word.length; i++){
+      let charToInsert = word[i]
+      if(!(charToInsert in curr.children)){
+        curr.children[charToInsert] = new Node() 
+      }
+      curr = curr.children[charToInsert]
+    }
+    curr.isWordEnd = true
+  }
+
+  contains(word){
+    let curr = this.root
+    for(let i = 0; i < word.length; i++){
+      let charToFind = word[i]
+      if(!(charToFind in curr.children)){
+        return false
+      }
+      curr = curr.children[charToFind]
+    }
+    return curr.isWordEnd
+  }
+
+  startsWithPrefix(prefix){
+    let curr = this.root
+    for(let i = 0; i < prefix.length; i++){
+      let charToFind = prefix[i]
+      if(!(charToFind in curr.children)){
+        return false
+      }
+      curr = curr.children[charToFind]
+    }
+    return true
+  }
+
+}
